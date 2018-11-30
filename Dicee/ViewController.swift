2792b137c15ddbc5167be3f5e211fc5b6bcf692c
Michael Ayodele <ayo10@yahoo.com>
2018-11-30 12:21:18 +0100

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Michael Ayodele on 30/11/2018.
+//  Copyright © 2018 Michael Ayodele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

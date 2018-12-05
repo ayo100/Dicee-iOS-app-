333fa1d3ec69d069e423f19c8b42875b112a4dda
Michael Ayodele <ayo10@yahoo.com>
2018-12-05 10:35:32 +0100

    iOS app called Dicee. It is to simulate the rolling of dual dice like in the casino

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        rollTheDice()
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        rollTheDice()
+    }
+    
+    func rollTheDice(){
+        //generate randomly two numbers
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        //display the dice image on the imageView based on the generated random numbers
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollTheDice()
+    }
 }
 
